ac43105cba89e5f204653aad2b9154b36bc6a422
Carlos Adolfo <adolfo.ggc@gmail.com>
2021-05-17 17:46:02 -0300

    Create order controller

---
+++ spec/requests/order_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe "Orders", type: :request do
+  describe "GET /index" do
+    it "returns http success" do
+      get "/order/index"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /show" do
+    it "returns http success" do
+      get "/order/show"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
