fafb2e5e6acbf10b2d33f6288246a930c5c2186b
Carlos Adolfo <adolfo.ggc@gmail.com>
2021-05-17 17:39:31 -0300

    Create model Order

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_16_123134) do
+ActiveRecord::Schema.define(version: 2021_05_17_203846) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "orders", force: :cascade do |t|
+    t.string "number"
+    t.decimal "delivery_fee"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.text "description"
