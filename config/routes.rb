f9086edeee481ed3b76043e071be2957e71eb1e0
Carlos Adolfo <adolfo.ggc@gmail.com>
2021-05-17 16:03:21 -0300

    Add update route for product controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
 	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 	post '/products', to: 'products#create'
+  post '/products/edit', to: 'products#update'
 end
