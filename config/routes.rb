4b3d646e4701a4bb83c8db99ace4ff6d0dd86b43
Carlos Adolfo <adolfo.ggc@gmail.com>
2021-05-17 22:39:06 -0300

    Renaming controller name: orders -> order

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get '/orders/index', to: 'orders#index'
-  get '/orders/:id', to: 'orders#show'
+  get '/orders/index', to: 'order#index'
+  get '/orders/:id', to: 'order#show'
 	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 	post '/products', to: 'products#create'
