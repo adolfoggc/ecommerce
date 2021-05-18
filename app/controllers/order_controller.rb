afe9f16326ba5f1988158ce35a71f8c84be11eb2
Carlos Adolfo <adolfo.ggc@gmail.com>
2021-05-18 08:52:18 -0300

    Create action on order controller: create

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -4,4 +4,16 @@ class OrderController < ApplicationController
 
   def show
   end
+
+  def create
+		@order = Order.new(order_params)
+
+		respond_to do |format|
+			if @order.save
+				format.json { render json: @order, status: :created }
+			else
+				format.json { render json: @order.errors, status: :unprocessable_entity }
+			end
+		end
+	end
 end
