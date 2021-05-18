e18807ffc069c79fa01bf89ae15e932477f15afb
Carlos Adolfo <adolfo.ggc@gmail.com>
2021-05-18 13:39:22 -0300

    Add after_create action into Order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,16 @@
 class Order < ApplicationRecord
-  has_many :product_orders
-  has_many :products, through: :product_orders
+	has_many :product_orders
+	has_many :products, through: :product_orders
 
-  attr_accessor :listed_products
+	after_create :create_related_product_order
+
+	attr_accessor :listed_products
+
+	def create_related_product_order
+		self.listed_products.each do |product_order, attributes|
+			po = ProductOrder.new(attributes)
+			po.order_id = self.id
+			po.save
+		end
+	end
 end
