be673b1f579e34a2f878af2440e1b9136a06bcd5
Carlos Adolfo <adolfo.ggc@gmail.com>
2021-05-18 13:15:50 -0300

    Removeprivate method: get_product_orders

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -9,7 +9,7 @@ class OrderController < ApplicationController
   def create
 		@order = Order.new(order_params)
 		product_orders = get_product_orders
-		
+
 
 		respond_to do |format|
 			if @order.save
@@ -26,9 +26,6 @@ class OrderController < ApplicationController
 			product_orders_attributes: [ :product_id, :final_price, :quantity ])
 	end
 
-	def get_product_orders
-		params[:order][:product_orders_attributes]
-	end
 
 	def set_order
 		@order = Order.find(params[:id])
