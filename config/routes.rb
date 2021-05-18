62154e4de0a9140e7821a227e9622685a7c03f55
Carlos Adolfo <adolfo.ggc@gmail.com>
2021-05-17 22:40:03 -0300

    Add new route to order controller: create

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get '/orders/index', to: 'order#index'
   get '/orders/:id', to: 'order#show'
+  post '/orders', to: 'order#create'
 	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 	post '/products', to: 'products#create'
