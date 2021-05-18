57343721297f08eaf01a6de57174119448239c6a
Carlos Adolfo <adolfo.ggc@gmail.com>
2021-05-18 13:56:12 -0300

    Add method 'order product price' into ProductOrder model

--- app/models/product_order.rb
+++ app/models/product_order.rb
@@ -1,4 +1,13 @@
 class ProductOrder < ApplicationRecord
   belongs_to :product
   belongs_to :order
+
+  def order_product_price
+    if self.final_price.nil?
+      price = self.product.price
+    else
+      price = self.final_price
+    end
+    return price*self.quantity
+  end
 end
