105d47eb9790314ec650e7958a6a6b415a07f553
Carlos Adolfo <adolfo.ggc@gmail.com>
2021-05-18 22:23:57 -0300

    Add action to Special Offer controller: create

--- app/controllers/special_offer_controller.rb
+++ app/controllers/special_offer_controller.rb
@@ -1,4 +1,16 @@
 class SpecialOfferController < ApplicationController
+	def create
+		@special_offer = SpecialOffer.new(special_offer_params)
+		
+		respond_to do |format|
+			if @special_offer.save
+				format.json { render json: @special_offer, status: :created }
+			else
+				format.json { render json: @special_offer.errors, status: :unprocessable_entity }
+			end
+		end
+	end
+	
 	private
 		def special_offer_params
 			params.require(:special_offer).permit(:product_id, :kind)
