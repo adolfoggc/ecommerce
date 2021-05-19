efd93732957446e2035bdc33778edae5aeeccad5
Carlos Adolfo <adolfo.ggc@gmail.com>
2021-05-19 09:06:41 -0300

    Insert kind description and a new method to special offer model: special_price

--- app/models/special_offer.rb
+++ app/models/special_offer.rb
@@ -1,8 +1,22 @@
 class SpecialOffer < ApplicationRecord
   belongs_to :product
 
+  # Kind of special offer defines the final price
+  # pay_x_get_y: If someone buys n or more units of a product, pays only n-1 units, where n >= 3
+  # friendly_discount: It's a 5% discount
+
   enum kind: {
     pay_x_get_y: 0,
     friendly_discount: 1
   }
+
+  def special_price price, quantity
+    if (self.kind == SpecialOffer.kinds[:pay_x_get_y]) && (quantity >= 3)
+      return price * (quantity - 1)
+    elsif self.kind == SpecialOffer.kinds[:friendly_discount]
+      return (price * quantity)*0.95
+    else
+      return (price * quantity
+    end
+  end
 end
