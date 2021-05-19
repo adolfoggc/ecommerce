f037c0e898741575a6d0e0cb13446891d45be170
Carlos Adolfo <adolfo.ggc@gmail.com>
2021-05-19 11:29:20 -0300

    Update product order method: order_product_price

--- app/models/product_order.rb
+++ app/models/product_order.rb
@@ -4,11 +4,11 @@ class ProductOrder < ApplicationRecord
   belongs_to :special_offer, optional: true
 
   def order_product_price()
-    if self.final_price.nil?
-      price = self.product.price
+    if self.special_offer.nil?
+      price = self.product.price * self.quantity
     else
-      price = self.final_price
+      price = self.special_offer.special_price( self.quantity)
     end
-    return price*self.quantity
+    return price
   end
 end
