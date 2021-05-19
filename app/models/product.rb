aaf195933b6a71d44dedf7f90daa5c0e032b8162
Carlos Adolfo <adolfo.ggc@gmail.com>
2021-05-19 13:12:15 -0300

    Update product method: total_order_amount

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,8 @@
 class Product < ApplicationRecord
+  has_many :product_orders
+	has_many :orders, through: :product_orders
+  has_many :special_offers
+
   validates :in_stock, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :bar_code, presence: :true, uniqueness: true
-
 end
