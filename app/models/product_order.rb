8a69460946056449ff7ad5693093960d0ac82c0a
Carlos Adolfo <adolfo.ggc@gmail.com>
2021-05-19 10:12:20 -0300

    Add an optional belongs to relation with special order into ProductOrder model

--- app/models/product_order.rb
+++ app/models/product_order.rb
@@ -1,8 +1,9 @@
 class ProductOrder < ApplicationRecord
   belongs_to :product
   belongs_to :order
+  belongs_to :special_offer, optional: true
 
-  def order_product_price
+  def order_product_price()
     if self.final_price.nil?
       price = self.product.price
     else
