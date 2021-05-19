c0ad1bab4cdbf4e6d50e337dee32b1c6738259f3
Carlos Adolfo <adolfo.ggc@gmail.com>
2021-05-19 13:03:35 -0300

    Rename method in ProductOrder model: order_product_price -> check_final_price

--- app/models/product_order.rb
+++ app/models/product_order.rb
@@ -2,13 +2,13 @@ class ProductOrder < ApplicationRecord
   belongs_to :product
   belongs_to :order
   belongs_to :special_offer, optional: true
-  before_save :order_product_price
+  before_save :check_final_price
 
-  def order_product_price()
+  def check_final_price
     if self.special_offer.nil?
       price = self.normal_price
     else
-      price = self.special_offer.special_price( self.quantity)
+      price = self.special_offer.special_price( self.quantity )
     end
     self.final_price = price
   end
@@ -16,4 +16,5 @@ class ProductOrder < ApplicationRecord
   def normal_price
     price = self.product.price * self.quantity
   end
+
 end
