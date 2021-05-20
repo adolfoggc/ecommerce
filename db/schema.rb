8fc61493b4dc529d2ba5e813adfc1fda2c63a49b
Carlos Adolfo <adolfo.ggc@gmail.com>
2021-05-19 22:20:36 -0300

    Try to use local images on products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_19_103001) do
+ActiveRecord::Schema.define(version: 2021_05_20_010237) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 2021_05_19_103001) do
     t.decimal "price", precision: 10, scale: 2
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_url"
   end
 
   create_table "special_offers", force: :cascade do |t|
