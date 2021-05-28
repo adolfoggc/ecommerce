b9bcff56ea8fd4b1b6a9a06cfbf35a741087f597
Carlos Adolfo <adolfo.ggc@gmail.com>
2021-05-28 08:20:23 -0300

    Define order#index as root_path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-	get '/orders/index', to: 'order#index'
+	root to: 'order#index'
 	get '/orders/:id', to: 'order#show'
 	post '/orders', to: 'order#create'
 	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
